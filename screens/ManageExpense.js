648b8915d1fe9ad21c3fd1aaa4714f3e3d89a936
koleks92 <koleks92@gmail.com>
2023-09-20 10:25:12 +0200

    Screens created, navigation packages

---
+++ screens/ManageExpense.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+function ManageExpense() {
+    return (
+        <View>
+            <Text>ManageExpensesExpense screen!</Text>
+        </View>
+    )
+};
+
+export default ManageExpense;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+function RecentExpenses() {
+    return (
+        <View>
+            <Text>RecentExpenses screen!</Text>
+        </View>
+    )
+};
+
+export default RecentExpenses;
