fbdc264773b9d4748a5956473140165e50eb07b9
koleks92 <koleks92@gmail.com>
2023-09-25 19:47:32 +0200

    Recent expenses fetch firebase

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,23 +1,25 @@
 import { View, Text } from 'react-native';
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDay } from '../util/date';
 import { fetchExpenses } from '../util/http';
 
 function RecentExpenses() {
-    const expensesCtx = useContext(ExpensesContext);
+    // const expensesCtx = useContext(ExpensesContext);
+    const [fetchedExpenses, setFetchedExpenses] = useState([]);
 
     useEffect(() => {
         async function getExpenses() {
             const expenses = await fetchExpenses();
+            setFetchedExpenses(expenses);
         }
 
         getExpenses();
     }, []);
 
 
-    const RecentExpenses = expensesCtx.expenses.filter((expense) => {
+    const RecentExpenses = fetchedExpenses.filter((expense) => {
         const today = new Date();
         const date7DaysAgo = getDateMinusDay(today, 7);
 
